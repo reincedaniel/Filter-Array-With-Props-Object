520eeccb1923a48e751ce0c51afe3b2f43b5ed57
Lourenço Carlos <reincedaniel@gmail.com>
2022-04-08 15:51:15 +0100

    feat: My filter

---
+++ dt.js
@@ -0,0 +1,422 @@
+const dt = [
+  {
+    id: "041d7f09-6878-40ec-94a2-2f71de551621",
+    created: "2022-03-31T10:59:23Z",
+    data: {
+      vagaSubmetida: "Director de Recursos Humanos",
+      nome: "Domingos Miguel Kiosa Da Silva",
+      email: "jorgekiosa@gmail.com",
+      telefone: 924292396,
+      telefoneAlternativo: 924292396,
+      paisDeRedisencia: "Angola",
+      nacionalidade: "Angola",
+      provincia: "Benguela",
+      distrito: "sdsdsdsd",
+      nivelAcademicoDataInicio: "2022-02-27T00:00:00Z",
+      nivelAcademicoDataFim: "2022-04-06T00:00:00Z",
+      nivelAcademicoHabiltacoesLiterarias: "Licenciatura",
+      nivelAcademicoAreaDeFormacao: "Administração e Serviços",
+      dominioLinguisticoIdioma: "Frances",
+      dominioLinguisticoCompetenciaOral: "Intermediário",
+      dominioLinguisticoCompetenciaLeitura: "Intermediário",
+      dominioLinguisticoCompetenciaEscrita: "Intermediário",
+      experienciaProfissionalEmpregoActual: "Sim",
+      anoExperiencia: 4,
+      experienciaProfissionalNomeEmpresa: "sdsdsdsd",
+      funcao: "sdsdsdsd",
+      informacoesParaTrabalhoGenero: "Feminino",
+      informacoesParaTrabalhoDisponibilidade: "Sim",
+      informacoesParaTrabalhoViagens: "Sim",
+      informacoesParaTrabalhoComoSeApercebeu: "Whatsapp",
+      informacoesParaTrabalhoSeOutroCanal: "",
+      vagaBreveDescricao: "sdsdsdsdsdsd",
+      vagaCv: [
+        {
+          id: "0ad228cf-013d-4d44-a518-b62ed61f17d7",
+          url: "https://cms.digitalfactory.co.ao/api/assets/bo-carrinho/0ad228cf-013d-4d44-a518-b62ed61f17d7/",
+          __typename: "Asset",
+        },
+      ],
+      __typename: "FormularioFlatDataDto",
+    },
+    __typename: "Formulario",
+  },
+  {
+    id: "2d983bd2-9d60-4c4e-9fa0-2780f6e82a0f",
+    created: "2022-03-31T11:00:16Z",
+    data: {
+      vagaSubmetida: "Técnico de Desenvolvimento e Recrutamento",
+      nome: "João de Carvalho",
+      email: "jorgekiosa@gmail.com",
+      telefone: 924292396,
+      telefoneAlternativo: 924292396,
+      paisDeRedisencia: "Angola",
+      nacionalidade: "Angola",
+      provincia: "Cabinda",
+      distrito: "rrtrere",
+      nivelAcademicoDataInicio: "2022-03-01T00:00:00Z",
+      nivelAcademicoDataFim: "2022-03-19T00:00:00Z",
+      nivelAcademicoHabiltacoesLiterarias: "Pós-Graduação",
+      nivelAcademicoAreaDeFormacao: "Novos Projectos",
+      dominioLinguisticoIdioma: "Inglês",
+      dominioLinguisticoCompetenciaOral: "Intermediário",
+      dominioLinguisticoCompetenciaLeitura: "Intermediário",
+      dominioLinguisticoCompetenciaEscrita: "Intermediário",
+      experienciaProfissionalEmpregoActual: "Sim",
+      anoExperiencia: 6,
+      experienciaProfissionalNomeEmpresa: "dfdfdfdf",
+      funcao: "",
+      informacoesParaTrabalhoGenero: "Masculino",
+      informacoesParaTrabalhoDisponibilidade: "Sim",
+      informacoesParaTrabalhoViagens: "Sim",
+      informacoesParaTrabalhoComoSeApercebeu: "Whatsapp",
+      informacoesParaTrabalhoSeOutroCanal: "",
+      vagaBreveDescricao: "effdddfddfdf",
+      vagaCv: [
+        {
+          id: "6acdde14-af21-4ba5-8e9e-4c736d278a48",
+          url: "https://cms.digitalfactory.co.ao/api/assets/bo-carrinho/6acdde14-af21-4ba5-8e9e-4c736d278a48/",
+          __typename: "Asset",
+        },
+      ],
+      __typename: "FormularioFlatDataDto",
+    },
+    __typename: "Formulario",
+  },
+  {
+    id: "774cad69-13a9-4bd9-aa9c-fa56b1f65309",
+    created: "2022-03-31T13:18:13Z",
+    data: {
+      vagaSubmetida: "Director de Recursos Humanos",
+      nome: "Domingos Miguel Kiosa Da Silva",
+      email: "jorgekiosa@gmail.com",
+      telefone: 924292396,
+      telefoneAlternativo: 944971654,
+      paisDeRedisencia: "Angola",
+      nacionalidade: "Portugal",
+      provincia: "Lunda Sul",
+      distrito: "sdsdsdsd",
+      nivelAcademicoDataInicio: "2022-03-01T00:00:00Z",
+      nivelAcademicoDataFim: "2022-04-09T00:00:00Z",
+      nivelAcademicoHabiltacoesLiterarias: "Licenciatura",
+      nivelAcademicoAreaDeFormacao: "Finanças e Contabilidade",
+      dominioLinguisticoIdioma: "Inglês",
+      dominioLinguisticoCompetenciaOral: "Intermediário",
+      dominioLinguisticoCompetenciaLeitura: "Intermediário",
+      dominioLinguisticoCompetenciaEscrita: "Intermediário",
+      experienciaProfissionalEmpregoActual: "Sim",
+      anoExperiencia: 4,
+      experienciaProfissionalNomeEmpresa: "sdsdsdsdsd",
+      funcao: "ddssdsdsd",
+      informacoesParaTrabalhoGenero: "Masculino",
+      informacoesParaTrabalhoDisponibilidade: "Sim",
+      informacoesParaTrabalhoViagens: "Sim",
+      informacoesParaTrabalhoComoSeApercebeu: "Facebook",
+      informacoesParaTrabalhoSeOutroCanal: "",
+      vagaBreveDescricao: "sdsdsdsdds",
+      vagaCv: [
+        {
+          id: "f1a2bd65-4daa-45ce-942e-514f2dd62ee8",
+          url: "https://cms.digitalfactory.co.ao/api/assets/bo-carrinho/f1a2bd65-4daa-45ce-942e-514f2dd62ee8/",
+          __typename: "Asset",
+        },
+      ],
+      __typename: "FormularioFlatDataDto",
+    },
+    __typename: "Formulario",
+  },
+  {
+    id: "28ab6b98-03db-464c-b49c-52be3a0b50fc",
+    created: "2022-03-31T13:20:46Z",
+    data: {
+      vagaSubmetida: "Técnico de Desenvolvimento e Recrutamento",
+      nome: "Domingos Miguel Kiosa Da Silva",
+      email: "jorgekiosa@gmail.com",
+      telefone: 924292396,
+      telefoneAlternativo: 924440471,
+      paisDeRedisencia: "Angola",
+      nacionalidade: "Angola",
+      provincia: "Namibe",
+      distrito: "dfddf",
+      nivelAcademicoDataInicio: "2022-02-27T00:00:00Z",
+      nivelAcademicoDataFim: "2022-04-09T00:00:00Z",
+      nivelAcademicoHabiltacoesLiterarias: "Mestrado",
+      nivelAcademicoAreaDeFormacao: "Administração e Serviços",
+      dominioLinguisticoIdioma: "Inglês",
+      dominioLinguisticoCompetenciaOral: "Intermediário",
+      dominioLinguisticoCompetenciaLeitura: "Intermediário",
+      dominioLinguisticoCompetenciaEscrita: "Intermediário",
+      experienciaProfissionalEmpregoActual: "Sim",
+      anoExperiencia: 10,
+      experienciaProfissionalNomeEmpresa: "dfdffdf",
+      funcao: "sdsdsdsd",
+      informacoesParaTrabalhoGenero: "Feminino",
+      informacoesParaTrabalhoDisponibilidade: "Sim",
+      informacoesParaTrabalhoViagens: "Sim",
+      informacoesParaTrabalhoComoSeApercebeu: "Instagram",
+      informacoesParaTrabalhoSeOutroCanal: "",
+      vagaBreveDescricao: "sdddfdf",
+      vagaCv: [
+        {
+          id: "e708d137-51e7-42a5-8c09-ad8adb38d86e",
+          url: "https://cms.digitalfactory.co.ao/api/assets/bo-carrinho/e708d137-51e7-42a5-8c09-ad8adb38d86e/",
+          __typename: "Asset",
+        },
+      ],
+      __typename: "FormularioFlatDataDto",
+    },
+    __typename: "Formulario",
+  },
+  {
+    id: "18d123b0-91cd-48e7-beef-84ab6272ab6c",
+    created: "2022-04-05T08:42:23Z",
+    data: {
+      vagaSubmetida: "Técnico de Data Center",
+      nome: "Jorge Miguel Kiosa da Silva",
+      email: "jorgekiosa@gmail.com",
+      telefone: 924292396,
+      telefoneAlternativo: 944971654,
+      paisDeRedisencia: "Angola",
+      nacionalidade: "Angola",
+      provincia: "Benguela",
+      distrito: "Benfica",
+      nivelAcademicoDataInicio: "2022-04-03T00:00:00Z",
+      nivelAcademicoDataFim: "2022-05-07T00:00:00Z",
+      nivelAcademicoHabiltacoesLiterarias: "Mestrado",
+      nivelAcademicoAreaDeFormacao: "Novos Projectos",
+      dominioLinguisticoIdioma: "Inglês",
+      dominioLinguisticoCompetenciaOral: "Intermediário",
+      dominioLinguisticoCompetenciaLeitura: "Intermediário",
+      dominioLinguisticoCompetenciaEscrita: "Intermediário",
+      experienciaProfissionalEmpregoActual: "Sim",
+      anoExperiencia: 5,
+      experienciaProfissionalNomeEmpresa: "Wico",
+      funcao: "Web Design",
+      informacoesParaTrabalhoGenero: "Masculino",
+      informacoesParaTrabalhoDisponibilidade: "Sim",
+      informacoesParaTrabalhoViagens: "Sim",
+      informacoesParaTrabalhoComoSeApercebeu: "Instagram",
+      informacoesParaTrabalhoSeOutroCanal: "",
+      vagaBreveDescricao: "Breve Apresentação",
+      vagaCv: [
+        {
+          id: "9f7e85de-99f1-4281-b70a-f0903169fab1",
+          url: "https://cms.digitalfactory.co.ao/api/assets/bo-carrinho/9f7e85de-99f1-4281-b70a-f0903169fab1/",
+          __typename: "Asset",
+        },
+      ],
+      __typename: "FormularioFlatDataDto",
+    },
+    __typename: "Formulario",
+  },
+  {
+    id: "949199de-79f5-45ef-a1c6-911de4d4fb96",
+    created: "2022-04-05T10:24:40Z",
+    data: {
+      vagaSubmetida: "Web Designer",
+      nome: "Domingos Miguel Kiosa Da Silva",
+      email: "jorgekiosa@gmail.com",
+      telefone: 924292396,
+      telefoneAlternativo: 924292396,
+      paisDeRedisencia: "Angola",
+      nacionalidade: "Angola",
+      provincia: "Kwanza Sul",
+      distrito: "Benfica",
+      nivelAcademicoDataInicio: "2022-04-07T00:00:00Z",
+      nivelAcademicoDataFim: "2022-04-30T00:00:00Z",
+      nivelAcademicoHabiltacoesLiterarias: "Licenciatura",
+      nivelAcademicoAreaDeFormacao: "Administração e Serviços",
+      dominioLinguisticoIdioma: "Inglês",
+      dominioLinguisticoCompetenciaOral: "Iniciante",
+      dominioLinguisticoCompetenciaLeitura: "Iniciante",
+      dominioLinguisticoCompetenciaEscrita: "Iniciante",
+      experienciaProfissionalEmpregoActual: "Sim",
+      anoExperiencia: 2,
+      experienciaProfissionalNomeEmpresa: "Venus Tecno",
+      funcao: "Web Design",
+      informacoesParaTrabalhoGenero: "Masculino",
+      informacoesParaTrabalhoDisponibilidade: "Sim",
+      informacoesParaTrabalhoViagens: "Sim",
+      informacoesParaTrabalhoComoSeApercebeu: "Facebook",
+      informacoesParaTrabalhoSeOutroCanal: "",
+      vagaBreveDescricao: "Breve apresentação",
+      vagaCv: [
+        {
+          id: "6acdde14-af21-4ba5-8e9e-4c736d278a48",
+          url: "https://cms.digitalfactory.co.ao/api/assets/bo-carrinho/6acdde14-af21-4ba5-8e9e-4c736d278a48/",
+          __typename: "Asset",
+        },
+      ],
+      __typename: "FormularioFlatDataDto",
+    },
+    __typename: "Formulario",
+  },
+];
+
+function replaceSpecialChars(str) {
+  // console.log("Dentro: ", str)
+  if (str != null || str != "")
+    return str.toString().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+  else return str;
+}
+
+function filterx(nome, informacoesParaTrabalhoGenero) {
+  return dt.filter((item) => {
+    return replaceSpecialChars(nome)
+      .toString()
+      .toLowerCase()
+      .split(" ")
+      .every(
+        (v) =>
+          nome &&
+          item.data &&
+          replaceSpecialChars(item.data.nome)
+            .toString()
+            .toLowerCase()
+            .includes(v) &&
+          (informacoesParaTrabalhoGenero == null ||
+            (informacoesParaTrabalhoGenero &&
+              item.data &&
+              replaceSpecialChars(item.data.informacoesParaTrabalhoGenero)
+                .toString()
+                .toLowerCase()
+                .includes(v)))
+      );
+  });
+}
+
+function filtery(dt,provincia, informacoesParaTrabalhoGenero, nivelAcademicoHabiltacoesLiterarias, dominioLinguisticoIdioma, vagaSubmetida) {
+  let data = [];
+  data = dt
+
+  if(provincia)
+    data = data.filter((item) => {
+        return replaceSpecialChars(provincia)
+        .toString()
+        .toLowerCase()
+        .split(" ")
+        .every(
+            (v) =>
+            provincia &&
+            item.data &&
+            replaceSpecialChars(item.data.provincia)
+                .toString()
+                .toLowerCase()
+                .includes(v)
+        );
+    });
+
+    if(informacoesParaTrabalhoGenero)
+    data = data.filter((item) => {
+        return replaceSpecialChars(informacoesParaTrabalhoGenero)
+        .toString()
+        .toLowerCase()
+        .split(" ")
+        .every(
+            (v) =>
+            informacoesParaTrabalhoGenero &&
+            item.data &&
+            replaceSpecialChars(item.data.informacoesParaTrabalhoGenero)
+                .toString()
+                .toLowerCase()
+                .includes(v)
+        );
+    });
+
+    if(nivelAcademicoHabiltacoesLiterarias)
+    data = data.filter((item) => {
+        return replaceSpecialChars(nivelAcademicoHabiltacoesLiterarias)
+        .toString()
+        .toLowerCase()
+        .split(" ")
+        .every(
+            (v) =>
+            nivelAcademicoHabiltacoesLiterarias &&
+            item.data &&
+            replaceSpecialChars(item.data.nivelAcademicoHabiltacoesLiterarias)
+                .toString()
+                .toLowerCase()
+                .includes(v)
+        );
+    });
+
+
+    if(dominioLinguisticoIdioma)
+    data = data.filter((item) => {
+        return replaceSpecialChars(dominioLinguisticoIdioma)
+        .toString()
+        .toLowerCase()
+        .split(" ")
+        .every(
+            (v) =>
+            dominioLinguisticoIdioma &&
+            item.data &&
+            replaceSpecialChars(item.data.dominioLinguisticoIdioma)
+                .toString()
+                .toLowerCase()
+                .includes(v)
+        );
+    });
+
+    if(vagaSubmetida)
+    data = data.filter((item) => {
+        return replaceSpecialChars(vagaSubmetida)
+        .toString()
+        .toLowerCase()
+        .split(" ")
+        .every(
+            (v) =>
+            vagaSubmetida &&
+            item.data &&
+            replaceSpecialChars(item.data.vagaSubmetida)
+                .toString()
+                .toLowerCase()
+                .includes(v)
+        );
+    });
+
+  return data;
+}
+
+function filterArray(array, filters) {
+  const filterKeys = Object.keys(filters);
+  return array.filter((item) => {
+    // validates all filter criteria
+    return filterKeys.every((key) => {
+      // ignores non-function predicates
+      if (typeof filters[key] !== "function") return true;
+      return filters[key](item[key]);
+    });
+  });
+}
+
+const multiFilter = (arr, filters) => {
+  let filterKeys = Object.keys(filters);
+  return arr.filter((eachObj) => {
+    return filterKeys.every((eachKey) => {
+      if (!filters[eachKey].length) {
+        return true; // passing an empty filter means that filter is ignored.
+      }
+      return filters[eachKey] === eachObj[eachKey];
+    });
+  });
+};
+
+const filters = {
+  data: (data) => data.nome == "Domingos",
+};
+/* 
+const filtered = filterArray(dt, filters);
+console.log("filterArray: ", filtered);
+
+console.log("--------------===================------------------");
+console.log("multiFilter: ", multiFilter(dt, filters));
+
+console.log("--------------===================------------------");
+console.log("filterx: ", filterx("João", "Masculino"));
+
+console.log("--------------===================------------------"); */
+
+
+console.log("filtery: ", filtery(dt,null, null, null, null,"Técnico de Data Center"));
+/*  console.log(filterx('João de Carvalho', null))  */
